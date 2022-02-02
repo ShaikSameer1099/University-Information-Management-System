e4a3a1107ee201bcf9ca55f32e5634e69f4fdc0c
ShaikSameer1099 <98880670+ShaikSameer1099@users.noreply.github.com>
2022-02-02 17:42:50 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,46 @@
+package com.springboot;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	@Column(name="name")
+	private String name;
+	@Id
+	@Column(name="email")
+	private String email;
+	@Column(name="phone")
+	private String phone;
+	@Column(name="message")
+	private String message;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
