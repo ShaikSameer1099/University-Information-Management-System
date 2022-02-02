9106597339350942022bb9c6dcc8076ff29bce4d
ShaikSameer1099 <98880670+ShaikSameer1099@users.noreply.github.com>
2022-02-02 18:15:40 +0530

    Delete CommentDao.java

--- CommentDao.java
+++
@@ -1,8 +0,0 @@
-package com.springboot;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CommentDao extends CrudRepository<Comment, String>{
-
-}
